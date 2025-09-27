f5226349d5824a84b3ed3d54336416f5cc4635b4
Misganaab Esayas <misganaab.esayas@du.edu.et>
2025-09-28 01:54:39 +0300

    basic operation

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,10 @@
+number1 = 10
+number2 = 5
+
+sum_result = number1 + number2
+subtract_result = number1 - number2
+product_result = number1 * number2
+
+print("Addition of", number1 ,"and", number2,"is", sum_result)
+print("Subtraction of", number1 ,"and", number2,"is", subtract_result)
+print("Multiplication of", number1 ,"and", number2,"is", product_result)
